6a4a92918fd141779d170d606f26585cdb31ea75
siwonpada <psw041115@gmail.com>
2023-03-11 03:20:42 +0900

    add login logic

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,19 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/global/entity/User.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios/dist';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AxiosError } from 'axios';
+import { catchError, firstValueFrom } from 'rxjs';
+import { User } from 'src/global/entity/User.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async login(authCode: string): Promise<{ jwt: string }> {
+    const params = {
+      auth_code: authCode,
+      client_id: this.configService.get<string>('IDP_CLIENT_ID'),
+      client_secret_key: this.configService.get<string>(
+        'IDP_CLIENT_SECRET_KEY',
+      ),
+    };
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get<{ jwt: string }>('https://api.idp.gistory.me/idp/get_token', {
+          params,
+        })
+        .pipe(
+          catchError((error: AxiosError) => {
+            console.log(error.response.data);
+            throw new UnauthorizedException();
+          }),
+        ),
+    );
+    return data;
+  }
+
+  async registerUser(jwt: string): Promise<void> {
+    return;
+  }
+}
