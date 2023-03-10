d0c6019c6db48008a17d2c0ce2ce50734200d687
siwonpada <psw041115@gmail.com>
2023-03-11 02:53:36 +0900

    add comment about getMealPlan

--- src/meal_plan/meal_plan.controller.ts
+++ src/meal_plan/meal_plan.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { UsePipes } from '@nestjs/common/decorators';
+import { ValidationPipe } from '@nestjs/common/pipes';
 import { MealPlan } from 'src/global/entity/MealPlan.entity';
 import { GetMealPlanDto } from './meal_plan.dto';
 import { MealPlanService } from './meal_plan.service';
 
 @Controller('meal_plan')
+@UsePipes(ValidationPipe)
 export class MealPlanController {
   constructor(private readonly mealPlanService: MealPlanService) {}
 
+  //급식 표는 로그인 여부와 상관없이 접근 가능
   @Get('')
   async getMealPlan(
     @Query() { date, max_list }: GetMealPlanDto,
